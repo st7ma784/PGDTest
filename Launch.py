81d360e9ce43ef8cdf0c35aeb003d56d0af050ce
steve <you@example.com>
2024-09-20 21:02:14 +0100

    typo darnit

--- Launch.py
+++ Launch.py
@@ -64,7 +64,7 @@ def train(config={
             callbacks=callbacks,
             gradient_clip_val=0.25,# Not supported for manual optimization
             fast_dev_run=False,
-            precision=p
+            precision=p,
             debug=config.get("debug",False),
     )
     if config["batch_size"] !=1:
