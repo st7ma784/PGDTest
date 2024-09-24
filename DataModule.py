0141775cd0fc070f9811f21fbab748b8efd4a5f7
steve mander <st7ma784@gmail.com>
2024-09-24 18:35:00 +0100

    fx

--- DataModule.py
+++ DataModule.py
@@ -485,7 +485,7 @@ class MyDataModule(pl.LightningDataModule):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=16 if not self.ISHEC else 2 ,pin_memory=not self.ISHEC,prefetch_factor=4 if not self.ISHEC else 1,drop_last=True)
 
     def val_dataloader(self):
-        return [DataLoader(dataset, batch_size=self.batch_size, shuffle=False, num_workers=16 if not self.ISHEC else 1, pin_memory=not self.ISHEC,prefetch_factor=4 if not self.ISHEC else 1,drop_last=True) for dataset in self.val_datasets]
+        return [DataLoader(dataset, batch_size=self.batch_size, shuffle=True, num_workers=16 if not self.ISHEC else 1, pin_memory=not self.ISHEC,prefetch_factor=4 if not self.ISHEC else 1,drop_last=True) for dataset in self.val_datasets]
 
     def test_dataloader(self):
         return [DataLoader(dataset, batch_size=self.batch_size, shuffle=False, num_workers=16 if not self.ISHEC else 1, pin_memory=not self.ISHEC,prefetch_factor=4 if not self.ISHEC else 1,drop_last=True) for dataset in self.val_datasets]
