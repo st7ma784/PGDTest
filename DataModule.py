380dd0e845005ad29966e931a06992bcb041c31b
steve mander <st7ma784@gmail.com>
2024-09-24 19:02:37 +0100

    fx

--- DataModule.py
+++ DataModule.py
@@ -157,8 +157,6 @@ class CustomtorchVisionDataset2(Dataset):
         except:
             print("Error in getting text")
             print("label:",label)
-            print("texts:",self.tokenized_texts)
-            print("idx:",idx)
             print("len of dataset:",len(self.dataset))
             print("len of texts:",len(self.tokenized_texts))
             # text="A picture of something"
