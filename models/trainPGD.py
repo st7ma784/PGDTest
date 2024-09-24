5f0daecaa95b9501608746692b61dd88f1b94e8a
steve mander <st7ma784@gmail.com>
2024-09-24 18:00:53 +0100

    fx

--- models/trainPGD.py
+++ models/trainPGD.py
@@ -188,8 +188,7 @@ class myLightningModule(LightningModule):
         delta=self.init_delta(X,epsilon)
         losses=[]
         self.model.eval()
-        scale_text_embed=self.model.encode_text(text_tokens)
-        scale_text_embed = scale_text_embed / scale_text_embed.norm(dim=-1, keepdim=True)
+        
         for _ in range(attack_iters):
             # output = model(normalize(X ))
             #prompted_images = self.prompter(normalize(delta + X ))
@@ -198,7 +197,8 @@ class myLightningModule(LightningModule):
             prompted_images = torch.div(torch.sub(new_images, self.mu_img), self.std_img) #normalize(new_images) but preserves grad
             img_embed=self.model.encode_image(prompted_images)
             img_embed_norm = img_embed / img_embed.norm(dim=-1, keepdim=True)
-
+            scale_text_embed=self.model.encode_text(text_tokens)
+            scale_text_embed = scale_text_embed / scale_text_embed.norm(dim=-1, keepdim=True)
             output = img_embed_norm @ scale_text_embed.t()
             loss = self.criterion(output, torch.arange(prompted_images.size(0), device=self.device))
             loss.backward()
@@ -211,9 +211,7 @@ class myLightningModule(LightningModule):
             d = clamp(d, self.lower_limit - x, self.upper_limit - x)
             delta.data[:, :, :, :] = d
             delta.grad.zero_()
-            scale_text_embed.grad.data.zero_()
-            
-            #zero the gradient of scale text embed 
+             
                         
         self.log("mean_attack_losses",sum(losses)/len(losses))
         self.log("max_attack_loss",max(losses))
