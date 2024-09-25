db6de905cad1a8312fd60945dc92a312434cf8c6
steve mander <st7ma784@gmail.com>
2024-09-25 12:14:30 +0100

    fx Launch

--- Launch.py
+++ Launch.py
@@ -72,7 +72,7 @@ def train(config={
 #### This is a wrapper to make sure we log with Weights and Biases, You'll need your own user for this.
 def wandbtrain(config=None,dir=None,devices=None,accelerator=None,Dataset=None):
 
-    USER="st7ma784" #<-----CHANGE ME
+    USER="PGNAttacks" #<-----CHANGE ME
     PROJECT="AllDataPGN"
     NAME="TestDeploy"
     import pytorch_lightning
