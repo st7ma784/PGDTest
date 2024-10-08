cb9d01da546122a5aca04f125d2b57d05685da88
John Doe <johndoe@example.com>
2024-10-08 10:55:56 +0100

    fx

--- Launch.py
+++ Launch.py
@@ -66,9 +66,8 @@ def train(config={
             precision=p,
             fast_dev_run=config.get("debug",False),
     )
-    if config["batch_size"] !=1:
 
-        trainer.fit(model,Dataset)
+    trainer.fit(model,Dataset)
 
     trainer.test(model,Dataset)
 #### This is a wrapper to make sure we log with Weights and Biases, You'll need your own user for this.
@@ -140,13 +139,14 @@ def SlurmRun(trialconfig):
                              '#SBATCH --cpus-per-task=8',
                              'export CONDADIR=/storage/hpc/46/manders3/conda4/open-ce',                                                     #<-----CHANGE ME
                              'export NCCL_SOCKET_IFNAME=enp0s31f6',
-                             'export WANDB_CACHE_DIR=$global_scratch', ])                                                 #<-----CHANGE ME])
+                             'export WANDB_CACHE_DIR=$global_scratch', 
+                             'export ISHEC=True',])                                                 #<-----CHANGE ME])
     sub_commands.extend([ '#SBATCH --{}={}\n'.format(cmd, value) for  (cmd, value) in slurm_commands.items()])
     sub_commands.extend([
         'export SLURM_NNODES=$SLURM_JOB_NUM_NODES',
         'export wandb=9cf7e97e2460c18a89429deed624ec1cbfb537bc',
         'export WANDB_API_KEY=9cf7e97e2460c18a89429deed624ec1cbfb537bc',
-        'export ISHEC=True',                                                                               #<-----CHANGE ME                                         
+                                                                                       #<-----CHANGE ME                                         
         'source /etc/profile',
         'module add opence',
         'conda activate $CONDADIR',
