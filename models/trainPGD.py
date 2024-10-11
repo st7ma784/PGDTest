82a00a90791776e74206854e6e965c5bb5d2efd6
John Doe <johndoe@example.com>
2024-10-11 15:16:29 +0100

    fx

--- models/trainPGD.py
+++ models/trainPGD.py
@@ -896,8 +896,8 @@ class myLightningModule(LightningModule):
 
 
             for dataset_idx in range(self.test_data_loader_count):
-                clean_files=filter(lambda x: x.startswith("cleanresults_{}_{}".format(version,dataset_idx)),cleanfilenames)
-                dirty_files=filter(lambda x: x.startswith("dirtyresults_{}_{}".format(version,dataset_idx)),dirtyfilenames)
+                clean_files=filter(lambda x: x.includes("_{}_pt".format(dataset_idx)),cleanfilenames)
+                dirty_files=filter(lambda x: x.startswith("_{}_pt".format(dataset_idx)),dirtyfilenames)
                 GoodLabels=[]
                 GoodLogits=[]
                 for file in clean_files:
