2a6ca97145611ef87bfdd4dd1d3607c5004ea236
steve <you@example.com>
2024-10-31 15:16:06 +0000

    fx

--- models/trainPGD.py
+++ models/trainPGD.py
@@ -1113,9 +1113,9 @@ class myLightningModule(LightningModule):
                     np.savez(dirtyPath,logits=logits,labels=labels,alphas=alpha,epsilons=epsilons,numsteps=numsteps)
                     # print("Saved dirty results to {}".format(dirtyPath))
                     dirtyidx+=1
-                if clear:
-                    EmptyCount+=1
-                    time.sleep(300)
+            if clear:
+                EmptyCount+=1
+                time.sleep(300)
 
                 # print("Saved results for dataset {}".format(dataset_idx))
             # if all([self.test_cleanresults[idx].empty() for idx in range(self.test_data_loader_count)]) and all([self.test_attackedresults[idx].empty() for idx in range(self.test_data_loader_count)]):
