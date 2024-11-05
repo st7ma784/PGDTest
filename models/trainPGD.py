c1a40c40db458ab9c6731cc49d1bac97c548cab4
Stephen Mander <s.mander3@lancaster.ac.uk>
2024-11-05 09:52:56 +0000

    rs

--- models/trainPGD.py
+++ models/trainPGD.py
@@ -576,21 +576,21 @@ class myLightningModule(LightningModule):
         output_prompt_adv = img_embed_norm @ scale_text_embed_norm.t()
 
         #compare class probabilities between output_prompt and output_prompt_adv
-        movement= output_prompt_adv - output_prompt
+        # movement= output_prompt_adv - output_prompt
         # # self.log("movement",movement)
         # # self.log("CleanSimilarity",output_prompt)
         # plot as heatmap using matplot and save as png
         # plot as scatter plot using matplot and save as png
-        fig=plt.figure()
-        plt.imshow(output_prompt.cpu().detach().numpy())
-        plt.colorbar()
-        plt.savefig("Cleanoutput_prompt_idx={}{}.png".format(batch_idx,dataloader_idx))
-        plt.close(fig)
-        fig=plt.figure()
-        plt.imshow(movement.cpu().detach().numpy())
-        plt.colorbar()
-        plt.savefig("movement_idx={}{}.png".format(batch_idx,dataloader_idx))
-        plt.close(fig)
+        # fig=plt.figure()
+        # plt.imshow(output_prompt.cpu().detach().numpy())
+        # plt.colorbar()
+        # plt.savefig("Cleanoutput_prompt_idx={}{}.png".format(batch_idx,dataloader_idx))
+        # plt.close(fig)
+        # fig=plt.figure()
+        # plt.imshow(movement.cpu().detach().numpy())
+        # plt.colorbar()
+        # plt.savefig("movement_idx={}{}.png".format(batch_idx,dataloader_idx))
+        # plt.close(fig)
         
 
 
