3aefb2a1e7a194b97754d9d41e18bc9637949789
Stephen Mander <s.mander3@lancaster.ac.uk>
2024-11-06 14:01:00 +0000

    fx

--- moveStorage.sh
+++ moveStorage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+FOLDER=$1 # Folder to move is the first argument
 
 # Loop through each folder in the current directory
-for dir in */; do
+for dir in FOLDER/*; do
     # Find all files in the directory
     find "$dir" -type f | while read -r file; do
         # Replace ./results with $global_scratch/results
